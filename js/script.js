f2985fa20530daac24badcde1ecd291742eb6860
Douglas Vini <douglasdannuz@gmail.com>
2024-07-02 19:20:53 -0300

    atualizar

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.querySelector('.menu-toggle');
     const nav = document.querySelector('nav');
 
-    menuToggle.addEventListener('click', function () {
-        menuToggle.querySelector('.hamburger').classList.toggle('active');
+    menuToggle.addEventListener('click', function() {
+        menuToggle.classList.toggle('active');
         nav.classList.toggle('active');
     });
 });
